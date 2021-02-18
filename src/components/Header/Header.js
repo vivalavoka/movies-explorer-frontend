712848d9c5c55036b9e140bb44094bb38ea31b49
Vladimir Nechitaylo <vladimir.nechitaylo@x5.ru>
2021-02-18 23:55:41 +0300

    Set sidebar format

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,8 +9,24 @@ export default function Headers(props) {
     <header className="header">
       <img src={logo} className="header__logo" alt="logo" />
 
-      {isLoggedIn &&
-      <nav>
+      <input class="header__menu-btn" type="checkbox" id="menu-btn" />
+      <label class="header__menu-icon" for="menu-btn">
+        <span class="header__navicon"></span>
+      </label>
+
+      <div className="sidebar">
+        <List className="header__navigation">
+          <li className="header__item"><a className="header__link" href='#'>Главная</a></li>
+          <li className="header__item"><a className="header__link header__link_active" href='#'>Фильмы</a></li>
+          <li className="header__item"><a className="header__link" href='#'>Сохраненные фильмы</a></li>
+        </List>
+        <div className="header__panel">
+          <Button className="header__profile-button" borderRadius="40">Аккаунт<span className="header__profile-icon"></span></Button>
+        </div>
+      </div>
+
+      {/* {isLoggedIn &&
+      <nav className="header__navigation">
         <List>
           <li className="header__item"><a className="header__link header__link_active" href='#'>Фильмы</a></li>
           <li className="header__item"><a className="header__link" href='#'>Сохраненные фильмы</a></li>
@@ -18,17 +34,13 @@ export default function Headers(props) {
       </nav>}
 
       <div className="header__panel">
-          <input class="header__menu-btn" type="checkbox" id="menu-btn" />
-          <label class="header__menu-icon" for="menu-btn">
-            <span class="header__navicon"></span>
-          </label>
           {isLoggedIn
             ? <Button className="header__profile-button" borderRadius="40">Аккаунт</Button>
             : <div>
               <Button className="header__sign-button">Регистрация</Button>
               <Button className="header__sign-button header__signin" color="green" borderRadius="3">Войти</Button>
             </div>}
-      </div>
+      </div> */}
     </header>
   );
 }
