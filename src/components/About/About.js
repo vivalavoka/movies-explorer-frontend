2366039de299e9575b096ecacc2702e420bff735
Vladimir Nechitaylo <vladimir.nechitaylo@x5.ru>
2021-02-20 10:36:56 +0300

    level-2: add timing into About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,6 +17,12 @@ export default function About(props) {
             <p className="about__article-description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
           </article>
         </div>
+        <div className="about__timing">
+          <span className="about__timing-block about__backend-timing">1 неделя</span>
+          <span className="about__timing-block about__frontend-timing">4 неделя</span>
+          <span className="about__timing-block about__timing-title">Back-end</span>
+          <span className="about__timing-block about__timing-title">Front-end</span>
+        </div>
       </div>
     </section >
   )
